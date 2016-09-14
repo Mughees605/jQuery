fdc10ac173e9ade2c9f3f44816cca660c672c667
mughees605 <mughees605@gmail.com>
2016-09-14 08:22:04 +0500

    on load and mouse event

--- Jquery/main.js
+++ Jquery/main.js
@@ -1,20 +1,22 @@
 $(document).ready(function(){
 $('.tour').on('click','button',function(){
-
-    var price = $(this).closest('.tour').data('discount');
-    var add = $('<button class="vacation"> $'+price+' complete </button>');
-    $(this).closest('.tour').append(add);
-    $(this).remove();
+var price = $(this).closest('.tour').data('discount');
+var add = $('<button class="vacation">From $'+price+'</button>');
+$(this).closest(".tour").append(add);
+$(this).remove();
 });
-$('#filter').on('click','.onsale-filter',function(){
-    $('.highlight').removeClass('highlight');
-    $('.tour').filter('.onsale').closest('div').addClass('highlight');
+$('#filter').on('click','button',function(){
+    $(".highlight").removeClass('highlight');
+$('.tour').filter('.onsale').closest('div').addClass('highlight');
+
 });
-$('#filter').on('click','.expiring-filter',function(){
-    $('.highlight').removeClass('highlight');
-    $('.tour').filter('.ex').closest('div').addClass('highlight');
-})
+function myFunction(){
+$(this).closest(".confirmation").find('.ticket').slideToggle();
+};
+$('.confirmation').on('click','button',myFunction)
+$('.confirmation').on('mouseenter','p',myFunction);
 
 
 
-})
+
+});
